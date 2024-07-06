c66e6d78e6f5df84fe05a30bf6702b3cac476c58
jtakumi <takumi87jn@gmail.com>
2024-07-06 22:51:16 +0900

    scroll screen added

---
+++ app/src/main/java/com/example/jetpacklist/data/LandmarkData.kt
@@ -0,0 +1,7 @@
+package com.example.jetpacklist.data
+
+data class LandmarkData(
+    val id:Int,
+    val name:String,
+    val description:String
+)
--- app/src/main/java/com/example/jetpacklist/views/LandmarkList.kt
+++ app/src/main/java/com/example/jetpacklist/views/LandmarkList.kt
@@ -1,19 +1,23 @@
 package com.example.jetpacklist.views
 
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpacklist.ui.theme.JetpackListTheme
 
-class LandmarkList(name: String, modifier: Modifier) {
-
-    @Composable
-    fun ListView(name: String, modifier: Modifier = Modifier) {
-       ListItem()
-    }
-}
 
 
     @Composable
@@ -24,6 +28,35 @@ class LandmarkList(name: String, modifier: Modifier) {
         )
     }
 
+@Composable
+fun LandmarkListView(name: String, modifier: Modifier) {
+    Column(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.background)
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+                .fillMaxWidth()
+                .background(color = Color.LightGray)
+                .padding(4.dp)
+                .height(60.dp)
+        )
+        repeat(100) {
+            Text(
+                text = "Item $it : name $name",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(4.dp)
+                    .height(48.dp)
+            )
+        }
+    }
+}
+
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
@@ -31,3 +64,12 @@ class LandmarkList(name: String, modifier: Modifier) {
             Greeting("Android")
         }
     }
+
+@Preview(showBackground = true)
+@Composable
+fun LandmarkListViewPreview() {
+    JetpackListTheme {
+        LandmarkListView("Android", modifier = Modifier)
+    }
+}
+
