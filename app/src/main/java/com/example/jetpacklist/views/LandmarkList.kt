aeed81e297c17f99e0b787959b8e0178ddb65bd0
jtakumi <takumi87jn@gmail.com>
2024-07-07 22:29:36 +0900

    add Column and Spacer

--- app/src/main/java/com/example/jetpacklist/views/LandmarkList.kt
+++ app/src/main/java/com/example/jetpacklist/views/LandmarkList.kt
@@ -1,7 +1,10 @@
 package com.example.jetpacklist.views
 
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
@@ -9,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpacklist.data.LandmarkData
@@ -20,6 +24,8 @@ import com.example.jetpacklist.viewmodel.LandmarkViewModel
 fun LandmarkList(viewModel: LandmarkViewModel) {
     val landmarks = viewModel.landmarks.observeAsState(initial = emptyList())
    LazyColumn {
+       item { Spacer(modifier = Modifier.padding(25.dp))}
+       item { Text(text = "Landmarks", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.headlineLarge) }
        items(landmarks.value.size) { index ->
            ListItem(landmark = landmarks.value[index])
        }
@@ -28,7 +34,10 @@ fun LandmarkList(viewModel: LandmarkViewModel) {
 
     @Composable
     fun ListItem(landmark: LandmarkData) {
-        Column(modifier = Modifier.padding(8.dp)) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.Gray)
+            .padding(16.dp)) {
             Text(text = landmark.name,style = MaterialTheme.typography.headlineSmall)
             Text(text = landmark.description,style = MaterialTheme.typography.bodyLarge)
             }
