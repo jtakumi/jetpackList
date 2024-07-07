d577745fbd515357b4025d426e03e422b4f8cb58
jtakumi <takumi87jn@gmail.com>
2024-07-07 22:00:12 +0900

    ViewModel

---
+++ app/src/main/java/com/example/jetpacklist/viewmodel/LandmarkViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.jetpacklist.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.jetpacklist.data.LandmarkData
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+import java.io.InputStreamReader
+
+class LandmarkViewModel(application: Application) : AndroidViewModel(application) {
+    private val _landmarks = MutableLiveData<List<LandmarkData>>()
+    val landmarks: LiveData<List<LandmarkData>> = _landmarks
+
+    init {
+        loadLandmarks()
+    }
+
+    private fun loadLandmarks() {
+        val assetManager = getApplication<Application>().assets
+        val inputStream = assetManager.open("Landmarks.json")
+        val jsonString =InputStreamReader(inputStream).use { it.readText() }
+        val landmarks =object : TypeToken<List<LandmarkData>>() {}.type
+        _landmarks.value = Gson().fromJson(jsonString, landmarks)
+    }
+
+}
