7b7db30c6e26fd891bec90c53df542d77e3956b8
jtakumi <takumi87jn@gmail.com>
2024-07-07 22:00:22 +0900

    ListView

--- app/src/main/java/com/example/jetpacklist/views/LandmarkList.kt
+++ app/src/main/java/com/example/jetpacklist/views/LandmarkList.kt
@@ -7,34 +7,49 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetpacklist.data.LandmarkData
 import com.example.jetpacklist.ui.theme.JetpackListTheme
+import com.example.jetpacklist.viewmodel.LandmarkViewModel
 
 
+
+@Composable
+fun LandmarkList(viewModel: LandmarkViewModel) {
+    val landmarks = viewModel.landmarks.observeAsState(initial = emptyList())
+   LazyColumn {
+       items(landmarks.value.size) { index ->
+           ListItem(landmark = landmarks.value[index])
+       }
+   }
+}
 
     @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-    }
+    fun ListItem(landmark: LandmarkData) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(text = landmark.name,style = MaterialTheme.typography.headlineSmall)
+            Text(text = landmark.description,style = MaterialTheme.typography.bodyLarge)
+            }
+        }
+
 
 @Composable
 fun LandmarkListView(name: String, modifier: Modifier) {
     Column(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background)
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         Text(
             text = "Hello $name!",
@@ -56,14 +71,6 @@ fun LandmarkListView(name: String, modifier: Modifier) {
         }
     }
 }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        JetpackListTheme {
-            Greeting("Android")
-        }
-    }
 
 @Preview(showBackground = true)
 @Composable
