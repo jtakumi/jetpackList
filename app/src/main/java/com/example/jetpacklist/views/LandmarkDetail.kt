56e4d2562d138df93537cf06ddd2a98e2afcdaeb
jtakumi <takumi87jn@gmail.com>
2024-07-10 22:55:29 +0900

    detail

--- app/src/main/java/com/example/jetpacklist/views/LandmarkDetail.kt
+++ app/src/main/java/com/example/jetpacklist/views/LandmarkDetail.kt
@@ -1,4 +1,17 @@
 package com.example.jetpacklist.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
 class LandmarkDetail {
+}
+
+@Composable
+fun LandmarkDetailView(landmarkId: Int,landmarkName: String,description:String) {
+    Column {
+        Text(text = landmarkId.toString())
+        Text(text = landmarkName)
+        Text(text = description)
+    }
 }
