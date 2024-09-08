a58219b3d311c7b87ff7271c8c24f144cdd6b106
jtakumi <takumi87jn@gmail.com>
2024-09-08 15:12:01 +0900

    load favorite data

--- app/src/main/java/com/example/jetpacklist/viewmodel/LandmarkViewModel.kt
+++ app/src/main/java/com/example/jetpacklist/viewmodel/LandmarkViewModel.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.jetpacklist.data.FavoriteData
 import com.example.jetpacklist.data.LandmarkData
 import com.example.jetpacklist.enum.getDefaultLanguage
 import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
@@ -14,9 +15,12 @@ import java.util.Locale
 class LandmarkViewModel(application: Application) : AndroidViewModel(application) {
     private val _landmarks = MutableLiveData<List<LandmarkData>>()
     val landmarks: LiveData<List<LandmarkData>> = _landmarks
+    private val _favorites = MutableLiveData<List<FavoriteData>>()
+    val favorites: LiveData<List<FavoriteData>> = _favorites
 
     init {
         loadLandmarks()
+        loadFavorites()
     }
 
     private fun loadLandmarks() {
@@ -35,5 +39,17 @@ class LandmarkViewModel(application: Application) : AndroidViewModel(application
         val language = locale.language
         Log.d("jetpackList",language)
        return getDefaultLanguage(language)
+    }
+    //お気に入りデータは言語が変わっても同じように表示させる
+    private fun loadFavorites() {
+        try {
+            val assetManager = getApplication<Application>().assets
+            val inputStream = assetManager.open("Favorites.json")
+            val jsonString = inputStream.bufferedReader().use { it.readText() }
+            val favorites = object : TypeToken<List<FavoriteData>>() {}.type
+            _favorites.value = Gson().fromJson(jsonString, favorites)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
