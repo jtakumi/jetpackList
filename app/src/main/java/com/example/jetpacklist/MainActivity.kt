2f36d061c41e34379ccf68ae963cd1c5a09652d1
jtakumi <takumi87jn@gmail.com>
2024-09-22 22:53:59 +0900

    MainActivity dataStore

--- app/src/main/java/com/example/jetpacklist/MainActivity.kt
+++ app/src/main/java/com/example/jetpacklist/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.jetpacklist
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.material3.Surface
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.compose.rememberNavController
 import com.example.jetpacklist.ui.theme.JetpackListTheme
 import com.example.jetpacklist.viewmodel.LandmarkViewModel
 import com.example.jetpacklist.views.SetLandmarkView
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "UserPreferences")
 
 class MainActivity : ComponentActivity() {
 
